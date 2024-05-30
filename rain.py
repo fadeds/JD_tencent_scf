6dbac2f12c571019c4d91be4600884320e0e55ea
fadeds <1154250793@qq.com>
2024-05-30 13:25:29 +0800

    Update rain.py
    改青龙版本

--- rain.py
+++ rain.py
@@ -1,18 +1,21 @@
 import requests
 import json
-import telepot
+import sendNotify
+from os import environ, system
+
+# import telepot
 
 #Api信息
-api_key = ''
-tgpush = ''
-msgid = ''
-bot_token = ''
+rain_api_key = ''
+
+rain_api_key = environ.get("rain_api_key") if environ.get("rain_api_key") else ""
+
 
 #请求用户信息
 url = "https://api.v2.rainyun.com/user/"
 payload={}
 headers_yh = {
-   'X-Api-Key': api_key
+   'X-Api-Key':rain_api_key
 }
 res_points = requests.request("GET", url, headers=headers_yh, data=payload)
 zh_json = res_points.json()
@@ -27,7 +30,7 @@ print('==============================')
 url_lqjf = 'https://api.v2.rainyun.com/user/reward/tasks'
 headers_lqjf = {
     'content-type':"application/json",
-    'X-Api-Key':api_key
+    'X-Api-Key':rain_api_key
     }
 body_lqjf = {
     "task_name" : '每日签到',
@@ -43,7 +46,6 @@ else:
     print(f'签到失败，返回值：{res_lqjf.text}')
 print('==============================')
 #推送
-if tgpush == 'true':
-    sendmessage = '''雨云自动签到Bot\n签到通知\n用户ID：{0}\n用户名：{1}\n签到前积分：{2}\n当前积分：{3}\nhttps://github.com/ZYGLQexplorer/RainYun-Checkin'''.format(ID, name, pointsbefore, points)
-    bot = telepot.Bot(bot_token)
-    bot.sendMessage(msgid, sendmessage, parse_mode=None, disable_web_page_preview=True, disable_notification=None, reply_to_message_id=None, reply_markup=None)
+
+sendmessage = '''雨云自动签到Bot\n签到通知\n用户ID：{0}\n用户名：{1}\n签到前积分：{2}\n当前积分：{3}\nhttps://github.com/ZYGLQexplorer/RainYun-Checkin'''.format(ID, name, pointsbefore, points)
+sendNotify.send('雨云签到',sendmessage)
